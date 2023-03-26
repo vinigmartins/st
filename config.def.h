74beb10c17c569977799e28f287a43ad4813923c
vinicius martins <vinicius.gmartins3@gmail.com>
2023-03-26 20:07:08 -0300

    style: ajust font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "iosevka:size=16:antialias=true:autohint=true";
+static char *font = "Iosevka:Bold:size=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
