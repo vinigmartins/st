45eb5a5900255baf7dd6195d504335642116d383
vinicius martins <vinicius.gmartins3@gmail.com>
2023-04-07 23:48:40 -0300

    style: change fonts

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka:Bold:size=12:antialias=true:autohint=true";
+/*static char *font = "Iosevka:Bold:size=12:antialias=true:autohint=true";*/
+static char *font = "Iosevka Term:SemiBold:Mono:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
